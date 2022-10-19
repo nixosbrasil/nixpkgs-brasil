7d08a8365838efcad1e6a20290794ef01b5299ce
lucasew <lucas59356@gmail.com>
2022-10-18 21:24:29 -0300

    add appimage-wrap
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- overlay.nix
+++ overlay.nix
@@ -2,4 +2,5 @@ self: super:  {
 
   hexchat-themes = super.callPackage ./pacotes/diversos/hexchat-themes { };
 
+  appimage-wrap = super.callPackage ./pacotes/utilitarios/appimage-wrap/default.nix { };
 }
