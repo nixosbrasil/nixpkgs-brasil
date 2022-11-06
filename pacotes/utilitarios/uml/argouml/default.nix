f827b3cf05844b3d9a7d1bc7632c197c9a1f725b
lucasew <lucas59356@gmail.com>
2022-11-05 22:41:16 -0300

    argouml: add icon
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- pacotes/utilitarios/uml/argouml/default.nix
+++ pacotes/utilitarios/uml/argouml/default.nix
@@ -6,24 +6,24 @@
 , copyDesktopItems
 , makeDesktopItem
 }:
-stdenv.mkDerivation {
+let
+  src = fetchzip {
+    url = "https://web.archive.org/web/20200701181113if_/http://argouml-downloads.tigris.org/nonav/argouml-0.34/ArgoUML-0.34.tar.gz";
+    sha256 = "1v2y8bmvl1fzcw1rmgw8jzyj4yhs0vik6kr0rrd9jcq2dikk3zz2";
+  };
+in stdenv.mkDerivation {
   pname = "argouml";
   version = "0.34";
 
   dontUnpack = true;
 
-  src = fetchzip {
-    url = "https://web.archive.org/web/20200701181113if_/http://argouml-downloads.tigris.org/nonav/argouml-0.34/ArgoUML-0.34.tar.gz";
-    sha256 = "1v2y8bmvl1fzcw1rmgw8jzyj4yhs0vik6kr0rrd9jcq2dikk3zz2";
-  };
-
-  nativeBuildInputs = [ makeWrapper copyDesktopItems ];
+    nativeBuildInputs = [ makeWrapper copyDesktopItems ];
 
   installPhase = ''
     runHook preInstall
 
     mkdir -p $out/bin
-    makeWrapper $src/argouml2.sh $out/bin/argouml  \
+    makeWrapper ${src}/argouml2.sh $out/bin/argouml  \
       --prefix PATH : ${lib.makeBinPath [ jre ]}
 
     runHook postInstall
@@ -34,7 +34,7 @@ stdenv.mkDerivation {
       name = "argouml";
       desktopName = "ArgoUML";
       exec = "argouml";
-      icon = "application-x-executable";
+      icon = "${src}/icon/ArgoIcon512x512.png";
       genericName = "Editor de diagramas UML";
       categories = [ "Office" ];
     })
