2da026dc2d46e6a6ce442bdee6035c23e8e2e7b4
lucasew <lucas59356@gmail.com>
2023-01-04 11:38:45 -0300

    wip.cockpit: checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- pacotes/wip/cockpit/default.nix
+++ pacotes/wip/cockpit/default.nix
@@ -20,28 +20,12 @@
 , libxslt
 , xmlto
 , python3Packages
-, docbook_xsl_ns
+, docbook_xsl
 , docbook_xml_dtd_43
 , nodejs
 , ripgrep
+, makeWrapper
 }:
-let
-  systemd_ctypes = python3Packages.buildPythonPackage rec {
-    pname = "systemd_ctypes";
-    version = "unstable-2023-01-03";
-    format = "pyproject";
-    src = fetchFromGitHub {
-      owner = "allisonkarlitskaya";
-      repo = "systemd_ctypes";
-      rev = "0238c864f937a259df9ddf28111deb9cfca82208";
-      sha256 = "sha256-el3Q2yTBMxcc22G4yQ4L+TiXB+iMY24HT+UpUF/0Rw0=";
-    };
-    propagatedBuildInputs = with python3Packages; [
-      flit-core
-      # systemd
-    ];
-  };
-in
 stdenv.mkDerivation rec {
   pname = "cockpit";
   version = "282.1";
@@ -55,16 +39,42 @@ stdenv.mkDerivation rec {
     deepClone = true;
   };
   # format = "pyproject";
-  nativeBuildInputs = [ pkg-config glib.dev pam.out python3Packages.setuptools autoreconfHook libxcrypt libxslt.bin xmlto gettext pam.out nodejs git ripgrep ];
-  buildInputs = [ glib udev json-glib gnutls krb5 polkit libssh ];
-  propagatedBuildInputs = [ systemd_ctypes ];
-  patches = [ ./disable-pam-check.patch ./nerf-node-modules.patch ];
+  nativeBuildInputs = [
+    pkg-config
+    glib.dev
+    pam.out
+    python3Packages.setuptools
+    autoreconfHook
+    libxcrypt
+    libxslt.bin
+    xmlto
+    gettext
+    pam.out
+    nodejs
+    git
+    ripgrep
+    docbook_xsl
+    docbook_xml_dtd_43
+    makeWrapper
+  ];
+  buildInputs = [
+    glib
+    udev
+    json-glib
+    gnutls
+    krb5
+    polkit
+    libssh
+  ];
+  patches = [
+    ./nerf-node-modules.patch
+  ];
   makeFlags = [
     "AM_DEFAULT_VERBOSITY=1"
   ];
   configureFlags = [
     "--enable-prefix-only=yes"
-    "--disable-pcp" # TODO: figure out how to package it's dependency
+    "--disable-pcp" # TODO: figure out how to package its dependency
   ];
   patchCodeImpurities = ''
     patchShebangs tools/escape-to-c
@@ -74,12 +84,6 @@ stdenv.mkDerivation rec {
     patchShebangs tools/webpack-make
     export HOME=$(mktemp -d)
     cp node_modules/.package-lock.json package-lock.json
-    # cp $src/.git . -r
-
-    substituteInPlace doc/guide/gtk-doc.xsl doc/man/Makefile-man.am doc/guide/*.xml \
-      --replace http://docbook.sourceforge.net/release/xsl/current ${docbook_xsl_ns}/xml/xsl/docbook \
-      --replace http://www.oasis-open.org/docbook/xml/4.3 ${docbook_xml_dtd_43}/xml/dtd/docbook
-    rg -F 'http://docbook.sourceforge.net' || echo nada feijoada
   '';
   generateVersionFile = ''
     echo "m4_define(VERSION_NUMBER, [${version}])" > version.m4
@@ -89,6 +93,11 @@ stdenv.mkDerivation rec {
     wrapProgram $out/bin/cockpit-bridge \
       --suffix LD_LIBRARY_PATH : /run/current-system/sw/lib \
       --suffix PYTHONPATH : $PYTHONPATH
+
+    patchShebangs $out/libexec/cockpit-certificate-helper
+    patchShebangs $out/libexec/cockpit-client
+    patchShebangs $out/libexec/cockpit-desktop
+    patchShebangs $out/share/cockpit/motd/update-motd
   '';
   checkPhase = ''
     make pytest
